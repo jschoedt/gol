e5234e6a41a10199fa76a585b77a4d8d00ed3c7c
Jens Schødt <jens.schoedt@gmail.com>
2020-05-25 16:31:48 +0200

    Added DefaultCtxLogger

---
+++ default.go
@@ -0,0 +1,41 @@
+package gol
+
+import (
+	"context"
+	"github.com/goburrow/gol"
+)
+
+// DefaultCtxLogger implements Logger interface.
+type DefaultCtxLogger struct {
+	gol.DefaultLogger
+}
+
+func NewCtxLogger(name string, parent *DefaultCtxLogger) *DefaultCtxLogger {
+	logger := gol.New(name, nil)
+	return &DefaultCtxLogger{*logger}
+}
+
+// TraceCtxf logs message at Trace level.
+func (logger *DefaultCtxLogger) TraceCtxf(ctx context.Context, format string, args ...interface{}) {
+	logger.Printf(gol.Trace, format, args)
+}
+
+// DebugCtxf logs message at Debug level.
+func (logger *DefaultCtxLogger) DebugCtxf(ctx context.Context, format string, args ...interface{}) {
+	logger.Printf(gol.Debug, format, args)
+}
+
+// InfoCtxf logs message at Info level.
+func (logger *DefaultCtxLogger) InfoCtxf(ctx context.Context, format string, args ...interface{}) {
+	logger.Printf(gol.Info, format, args)
+}
+
+// WarnCtxf logs message at Warning level.
+func (logger *DefaultCtxLogger) WarnCtxf(ctx context.Context, format string, args ...interface{}) {
+	logger.Printf(gol.Warn, format, args)
+}
+
+// ErrorCtxf logs message at Error level.
+func (logger *DefaultCtxLogger) ErrorCtxf(ctx context.Context, format string, args ...interface{}) {
+	logger.Printf(gol.Error, format, args)
+}
